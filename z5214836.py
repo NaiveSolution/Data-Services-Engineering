eb83db3c3a7ac7b11f058d9e99e2dbdda527db54
Tariq <52275187+NaiveSolution@users.noreply.github.com>
2019-10-14 21:36:39 +1100

    question3

--- z5214836.py
+++ z5214836.py
@@ -5,11 +5,13 @@ import numpy as np
 pd.option_context('display.max_columns', None, 'display.max_rows', None)
 
 def read_csv_files(file1, file2):
+
     summer_df = pd.read_csv('Olympics_dataset1.csv')
     winter_df = pd.read_csv('Olympics_dataset2.csv')
     return summer_df, winter_df
 
 def join_and_clean_tables(summer_df, winter_df):
+
     summer_df = summer_df.drop(axis=0,index=0)
     total_df = pd.merge(summer_df, winter_df, how='left', on=['Team'])
     total_df = total_df.drop(total_df.columns[12:], axis=1)
@@ -17,27 +19,38 @@ def join_and_clean_tables(summer_df, winter_df):
     return total_df
     
 def question_1():
+
     print("--------------- question_1 ---------------")
     summer_df, winter_df = read_csv_files('Olympics_dataset1.csv','Olympics_dataset2.csv')
-    total_df = join_and_clean_tables(summer_df, winter_df)
-    total_df = total_df.set_index('Country')
-    total_df = total_df.drop(index='Totals',axis=0)
-    total_df = total_df.iloc[0:5]
-    print(total_df.to_string())
+    q1_df = join_and_clean_tables(summer_df, winter_df)
+    q1_df = q1_df.set_index('Country')
+    q1_df = q1_df.drop(index='Totals',axis=0)
+    print(q1_df.iloc[0:5].to_string())
 
 
-def question_2():
-    print("--------------- question_2 ---------------")
-    summer_df, winter_df = read_csv_files('Olympics_dataset1.csv','Olympics_dataset2.csv')
-    total_df = join_and_clean_tables(summer_df, winter_df)
-    total_df['Country'] = total_df['Country'].apply(lambda x: x.lstrip().split(' ')[0])
-    total_df = total_df.set_index('Country')
-    total_df = total_df.iloc[0:5]
-    print(total_df.to_string())
+def question_2(print_val):
     
-def question_3():
-    print("--------------- question_3 ---------------")
-    pass
+    summer_df, winter_df = read_csv_files('Olympics_dataset1.csv','Olympics_dataset2.csv')
+    q2_df = join_and_clean_tables(summer_df, winter_df)
+    q2_df['Country'] = q2_df['Country'].apply(lambda x: x.lstrip().split(' ')[0])
+    q2_df = q2_df.set_index('Country')
+    columns = ['summer_rubbish', 'summer_total' , 'winter_total']
+    q2_df = q2_df.drop(columns, axis=1)
+    if print_val:
+        print("--------------- question_2 ---------------")
+        print(q2_df.iloc[0:5].to_string())
+
+    return q2_df
+
+def question_3(print_val):
+    
+    q3_df = question_2(None)
+    q3_df.dropna(axis=0, how='all')
+    if print_val:
+        print("--------------- question_3 ---------------")
+        print(q3_df.iloc[-10:].to_string())
+
+    return q3_df
 
 def question_4():
     print("--------------- question_4 ---------------")
@@ -69,8 +82,8 @@ def question_10():
 
 if __name__ == "__main__":
     question_1()
-    question_2()
-    question_3()
+    q2_df = question_2(1)
+    q3_df = question_3(1)
     question_4()
     question_5()
     question_6()
